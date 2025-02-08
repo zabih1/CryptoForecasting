9582eebe9094c36f6afd4de33e029eaa2fc47b1b
zabih ullah <zabihullah18381@gmail.com>
2025-02-08 11:04:26 +0500

    inferencing

--- ML/ML_inference.py
+++ ML/ML_inference.py
@@ -26,9 +26,7 @@ logging.basicConfig(
 
 # ========================== Model and Scaler Loading Functions ============================
 def load_model(model_path):
-    """
-    Load the trained model from the given path.
-    """
+
     with open(model_path, "rb") as f:
         model = pickle.load(f)
     return model
